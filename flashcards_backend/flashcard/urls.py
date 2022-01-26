84013e75cdb63c87c5d750f64bb7cf232a7ca14b
Audrey Schmidt <audsch@outlook.com>
2022-01-26 11:01:57 -0600

    added CRUD functions in views.py and mapped in urls.py for Flashcard model

--- flashcards_backend/flashcard/urls.py
+++ flashcards_backend/flashcard/urls.py
@@ -6,4 +6,8 @@ urlpatterns = [
     path('addcollection/', views.add_collection),
     path('deletecollection/<int:collection_id>/', views.delete_collection),
     path('editcollection/<int:collection_id>/', views.update_collection),
+    path('allflashcards/<int:collection_id>/', views.get_all_flashcards),
+    path('addflashcard/', views.add_flashcard),
+    path('deleteflashcard/<int:flashcard_id>/', views.delete_flashcard),
+    path('editflashcard/<int:flashcard_id>/', views.update_flashcard),
 ]
--- flashcards_backend/flashcard/views.py
+++ flashcards_backend/flashcard/views.py
@@ -15,6 +15,7 @@ from .serializers import UserSerializer
 # Create your views here.
 #!all temporarily allow any
 
+#collections
 @api_view(['GET'])
 @permission_classes([AllowAny])
 def get_all_collections(request):
@@ -36,7 +37,7 @@ def add_collection(request):
 def delete_collection(request, collection_id):
     collection = Collection.objects.get(id=collection_id)
     if request.user.id == collection.user.id:
-        serializer = CollectionSerializer(collection, many=False) #will display deleted song
+        serializer = CollectionSerializer(collection, many=False) #will display deleted collection
         collection.delete()
         return Response(serializer.data, status=status.HTTP_204_NO_CONTENT)
     else:
@@ -47,6 +48,44 @@ def delete_collection(request, collection_id):
 def update_collection(request, collection_id):
     collection = Collection.objects.get(id=collection_id)
     serializer = CollectionSerializer(collection, data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+#flashcards
+@api_view(['GET'])
+@permission_classes([AllowAny])
+def get_all_flashcards(request, collection_id):
+    flashcards = Flashcard.objects.filter(collection_id=collection_id)
+    serializer = FlashcardSerializer(flashcards, many=True)
+    return Response(serializer.data)
+
+@api_view(['POST'])
+@permission_classes([AllowAny])
+def add_flashcard(request):
+    serializer = FlashcardSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save(user=request.user)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['DELETE'])
+@permission_classes([AllowAny])
+def delete_flashcard(request, flashcard_id):
+    flashcard = Flashcard.objects.get(id=flashcard_id)
+    if request.user.id == flashcard.user.id:
+        serializer = FlashcardSerializer(flashcard, many=False) #will display deleted flashcard
+        flashcard.delete()
+        return Response(serializer.data, status=status.HTTP_204_NO_CONTENT)
+    else:
+        return Response(status=status.HTTP_401_UNAUTHORIZED)
+
+@api_view(['PUT'])
+@permission_classes([AllowAny])
+def update_flashcard(request, flashcard_id):
+    flashcard = Flashcard.objects.get(id=flashcard_id)
+    serializer = FlashcardSerializer(flashcard, data=request.data)
     if serializer.is_valid():
         serializer.save()
         return Response(serializer.data)
